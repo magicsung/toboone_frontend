d0e30397b29bf2013ea10a67277710653bee0318
Jack Sung <magicsung@gmail.com>
2016-05-13 23:55:07 +0800

    use pure css toggle menu.

--- js/src/main.js
+++ js/src/main.js
@@ -1,30 +1,6 @@
 "use strict";
 
 $( document ).ready(function() {
-  $("#xs-menu-action").click(function(event) {
-    event.preventDefault();
-    toggleMenu();
-  });
-});
-
-function toggleMenu(e) {
-  $('html').toggleClass("noscroll");
-  if ( $("#xs-menu-action").attr("alt") == "" ) {
-    $("#xs-menu-action").attr("alt", "active").addClass("active");
-    $('#sidebar').removeClass("hidden-xs");
-    $('#sidebar').addClass('slideInLeft' + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-      $(this).removeClass('slideInLeft animated');
-    });
-  } else {
-    $("#xs-menu-action").attr("alt", "").removeClass("active");
-    $('#sidebar').addClass('slideOutLeft' + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-      $(this).removeClass('slideOutLeft animated');
-      $('#sidebar').addClass("hidden-xs");
-    });
-  }
-};
-
-$( document ).ready(function() {
   $('.popover').on('click', function(event) {
     event.preventDefault();
     $(this).toggleClass('open');
